ce1071b95b7fe2866a0c65412cea6255b1aa7425
sebastiantroncoso <sctroncoso10@gmail.com>
2020-12-14 12:34:38 -0500

    Finished Hotel Schema in SQL

---
+++ Sebastian-HotelDB.sql
@@ -0,0 +1,63 @@
+
+drop database HotelSchema;
+create database HotelSchema;
+use HotelSchema;
+
+create table Room (
+	RoomNumber int(4) primary key not null,
+    `Type` varchar(10),
+    ADAAccessible bool not null,
+    StandardOccupancy int(4),
+    MaximumOccupancy int(4),
+    BasePrice decimal(6,2),
+    ExtraPerson int(2) null
+);
+
+create table Amenity(
+	AmenityId int primary key not null auto_increment,
+    AmenityName varchar(20) not null,
+    ChargePerAmenity decimal(2,2) null
+);
+
+create table Guest (
+	GuestId int primary key not null auto_increment,
+    FirstName varchar(50) not null,
+    LastName varchar(50) not null,
+    Address varchar(30) not null,
+    City varchar(30) not null,
+    State varchar(2) not null,
+    Zip int not null,
+    Phone varchar(15) not null
+);
+
+
+create table RoomAmenity (
+	AmenityId int not null auto_increment,
+    RoomNumber int(4) not null,
+    
+    primary key pk_RoomAmenity(AmenityId, RoomNumber),
+    foreign key fk_RoomAmenity_Room(RoomNumber)
+		references Room(RoomNumber),
+	foreign key fk_RoomAmenity_Amenity(AmenityId)
+		references Amenity(AmenityId)
+);
+
+				
+create table Reservation(
+
+	ReservationId int(8) primary key not null auto_increment,
+	GuestId int not null,
+    RoomNumber int not null,
+    foreign key fk_Reservation_Room(RoomNumber)
+		references Room(RoomNumber),
+    foreign key fk_Reservation_Guest(GuestId)
+		references Guest(GuestId) on delete cascade,
+	Adults int,
+    Children int,
+    StartDate date,
+    EndDate date,
+    TotalCost decimal(6,2)
+);
+
+
+
---
+++ Sebastian-HotelQueries.sql
@@ -0,0 +1,58 @@
+use HotelSchema;
+
+-- 1. Query that returns a list of reservations ending with July 2023, including guest name, room number, and reservations dates
+SELECT Guest.FirstName, Reservation.RoomNumber, Reservation.StartDate, Reservation.EndDate
+FROM Reservation
+	LEFT JOIN Guest ON
+		Reservation.GuestId = Guest.GuestId
+WHERE EndDate LIKE '2023-07-%';
+-- 2. Query that returns a list of all reservations for rooms with jacuzzi, guest name, room number, and dates of reservation
+SELECT Guest.FirstName, Reservation.RoomNumber, Reservation.StartDate, Reservation.EndDate, Amenity.AmenityName
+FROM Reservation
+	LEFT JOIN Guest ON
+		Reservation.GuestId = Guest.GuestId
+	RIGHT JOIN RoomAmenity ON
+		Reservation.RoomNumber = RoomAmenity.RoomNumber
+        LEFT JOIN Amenity ON
+		RoomAmenity.AmenityId = Amenity.AmenityId
+WHERE Amenity.AmenityName = 'Jacuzzi';
+        
+-- 3 Query that returns all the rooms reserved for a specific guest, including name, rooms reserved, reservation dates,  and number of people in reservation
+
+SELECT Guest.FirstName, Reservation.RoomNumber, Reservation.StartDate, Reservation.EndDate, Reservation.Adults, Reservation.Children
+FROM Reservation
+	LEFT JOIN Guest ON
+		Reservation.GuestId = Guest.GuestId
+WHERE Guest.FirstName = 'Karie';
+
+-- 4 Query return a list of rooms, reservations ID, cost of reservation. 
+
+SELECT Reservation.RoomNumber, Reservation.ReservationId, Reservation.TotalCost
+FROM Reservation;
+
+-- 5 Quert returns all the rooms accommodating at least three guests and reserved any date in April 2023 (Not that Jeremiah is not in the list)
+SELECT Reservation.RoomNumber, Reservation.StartDate, Reservation.EndDate, Room.MaximumOccupancy, SUM(Reservation.Adults + Reservation.Children) AS NumberOfGuests
+FROM Reservation
+	LEFT JOIN Room ON
+		Reservation.RoomNumber = Room.RoomNumber
+			AND Room.MaximumOccupancy > 3
+WHERE Reservation.EndDate LIKE '2023-04-%' OR Reservation.StartDate LIKE '2023-04-%'
+GROUP BY Reservation.RoomNumber, Reservation.StartDate, Reservation.EndDate
+HAVING SUM(Reservation.Adults + Reservation.Children) > 3;
+
+-- 6 Returns a list of all guest names and number of reservations per guest, sorted from morst reservations and then by guest last name
+SELECT Guest.FirstName, Guest.LastName, COUNT(Guest.FirstName) AS NumberOfReservations
+FROM Reservation
+LEFT JOIN Guest ON
+	Reservation.GuestId = Guest.GuestId
+GROUP BY Guest.FirstName, Guest.LastName
+ORDER BY COUNT(Guest.FirstName) DESC, Guest.LastName;
+
+-- 7 Returns the name, address, and phone number of the guest vased on their phone number
+SELECT Guest.FirstName, Guest.LastName, Guest.Address, Guest.City, Guest.State, Guest.Phone
+FROM Guest
+WHERE Guest.Phone = '(123) 456-7890';
+
+
+
+
